b0d591e96f65be0eb3b808eb0d576d33f176bed9
Lucas Tomkow <49009354+lucas-pohl-tomko@users.noreply.github.com>
2021-09-29 14:07:30 -0300

    More courses :+1:

--- Pluralsight-vuejs-fundamentals/build-a-bot/src/App.vue
+++ Pluralsight-vuejs-fundamentals/build-a-bot/src/App.vue
@@ -1,20 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item">
+            <img src="@/assets/build-a-bot-logo.png" alt="" class="logo" />
+            Build-a-Bot
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <RobotBuilder />
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+// import HomePage from './home/HomePage.vue';
+import RobotBuilder from './build/RobotBuilder.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    RobotBuilder,
   },
 };
 </script>
 
 <style>
+body {
+  background: linear-gradient(to bottom, #555, #999);
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +40,31 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+main {
+  margin: 0 auto;
+  padding: 30px;
+  background-color: white;
+  width: 1024px;
+  min-height: 300px;
+}
+header {
+  background-color: #999;
+  width: 1084px;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
 }
 </style>
