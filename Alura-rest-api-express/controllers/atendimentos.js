28b48099bd13f5465743444e83267a5e64ef93eb
Lucas Tomkow <49009354+lucas-pohl-tomko@users.noreply.github.com>
2021-10-05 14:15:16 -0300

    Finishing the first Alura Node Rest API course and  uploading the files to the second one :sailboat:

--- Alura-rest-api-express/controllers/atendimentos.js
+++ Alura-rest-api-express/controllers/atendimentos.js
@@ -1,6 +1,32 @@
+const Atendimento = require('../models/atendimentos')
+
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('Voce esta na rota de atendimentos, realizando um get'))
+    app.get('/atendimentos', (req, res) => {
+        Atendimento.lista(res)
+    })
 
-    app.post('/atendimentos', (req, res) => res.send('voce esta na rota de atendimentos, realizando um post'))
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
 
+        Atendimento.buscaPorId(id, res)
+    })
+
+    app.post('/atendimentos', (req, res) => {
+        const atendimento = req.body
+
+        Atendimento.adiciona(atendimento, res)
+    })
+
+    app.patch('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        const valores = req.body
+
+        Atendimento.altera(id, valores, res)
+    })
+
+    app.delete('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+
+        Atendimento.deleta(id, res)
+    })
 }
