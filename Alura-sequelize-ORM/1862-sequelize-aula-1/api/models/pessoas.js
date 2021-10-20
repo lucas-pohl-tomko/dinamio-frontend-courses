3733e2e0347c008dc4d12fee2995540a66563da6
lucas-pohl-tomko <lucas.pohl.tomko@gmail.com>
2021-10-20 14:38:28 -0300

    courses, final alura node courses and a tutorial on JWT auth

---
+++ Alura-sequelize-ORM/1862-sequelize-aula-1/api/models/pessoas.js
@@ -0,0 +1,26 @@
+'use strict'
+module.exports = (sequelize, DataTypes) => {
+  const Pessoas = sequelize.define('Pessoas', {
+    nome: DataTypes.STRING,
+    ativo: DataTypes.BOOLEAN,
+    email: DataTypes.STRING,
+    role: DataTypes.STRING
+  }, { 
+    paranoid: true,
+    defaultScope: {
+      where: {
+        ativo: true
+      }
+    }
+   })
+  Pessoas.associate = function(models) {
+    Pessoas.hasMany(models.Turmas, {
+      foreignKey: 'docente_id'
+    }) 
+    Pessoas.hasMany(models.Matriculas, {
+      foreignKey: 'estudante_id'
+    })
+
+  }
+  return Pessoas
+}
